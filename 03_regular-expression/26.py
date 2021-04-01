e0e6fda517a3d488d5c9fb8d78d1cae5bf356052
suzuk <suzukinoshochang@gmail.com>
2021-04-02 00:19:09 +0900

    add new files

---
+++ 03_regular-expression/26.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import re
+import pprint
+
+def remove_emphasis(value):
+    return re.sub(r'\'{2,5}','',value)
+
+def input_basic_dict(datalines, start_id, basic_dict):
+    for line in lines[start_id+1:]:
+        if re.match(r'^\|', line):
+            field = re.search(r'\|(\w*)', line).group(1)
+            value = re.search(r'=\s?(.*)', line).group(1)
+            basic_dict[field] = remove_emphasis(value)
+        elif re.match(r'\*+', line):
+            basic_dict[field] += remove_emphasis(line)
+        elif re.match(r'\}\}', line):
+            break
+        else:
+            continue
+
+#main
+data = pd.read_json("jawiki-country.json.gz", lines=True)
+lines = data[data['title']=='イギリス']['text'].values[0].splitlines()
+basic_dict = {}
+for i, ls in enumerate(lines):
+    if re.match(r'\{\{基礎情報', ls):
+        input_basic_dict(lines, i, basic_dict)
+        break
+pprint.pprint(basic_dict)
---
+++ 03_regular-expression/27.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import re
+import pprint
+
+def remove_emph_link(value):
+    value = re.sub(r'\'{2,5}','',value)
+    value = re.sub(r'(\[\[[^|\]]+\|)','',value)
+    value = re.sub(r'((\[\[)|(\]\]))','',value)
+    return value
+
+def input_basic_dict(datalines, start_id, basic_dict):
+    for line in lines[start_id+1:]:
+        if re.match(r'^\|', line):
+            field = re.search(r'\|(\w*)', line).group(1)
+            value = re.search(r'=\s?(.*)', line).group(1)
+            basic_dict[field] = remove_emph_link(value)
+        elif re.match(r'\*+', line):
+            basic_dict[field] += remove_emph_link(line)
+        elif re.match(r'\}\}', line):
+            break
+        else:
+            continue
+
+#main
+data = pd.read_json("jawiki-country.json.gz", lines=True)
+lines = data[data['title']=='イギリス']['text'].values[0].splitlines()
+basic_dict = {}
+for i, ls in enumerate(lines):
+    if re.match(r'\{\{基礎情報', ls):
+        input_basic_dict(lines, i, basic_dict)
+        break
+pprint.pprint(basic_dict)
